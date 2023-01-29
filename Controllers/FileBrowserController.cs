e4f328ba94b536cbe2eb07d4ab24386441b33dde
lcc206 <1402080954@qq.com>
2023-01-29 23:49:09 +0800

    Add FileBrowser

---
+++ Controllers/FileBrowserController.cs
@@ -0,0 +1,53 @@
+﻿using ByteSizeLib;
+using HostServer.Extentions;
+using HostServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HostServer.Controllers;
+
+public class FileBrowserController : Controller
+{
+    [BindProperty]
+    public string BasePath { get; set; } = @"D:/";
+
+    [HttpGet]
+    public IActionResult Index(string? relativePath)
+    {
+        FileBrowserUnit browser = new()
+        {
+            BasePath = Path.GetFullPath(BasePath).UnifySlash(),
+            RelativePath = relativePath is null ? "" : relativePath.UnifySlash()
+        };
+
+        var dirs = Directory.GetDirectories(browser.Path!);
+        var files = Directory.GetFiles(browser.Path!);
+
+        foreach (var dir in dirs)
+        {
+            if (new FileInfo(dir).Attributes.HasFlag(FileAttributes.Hidden) is true)
+                continue;
+
+            var directoryUnit = new DirectoryUnit
+            {
+                RelativePath = Path.GetFileName(dir),
+                ModifyTime = Directory.GetLastWriteTime(dir)
+            };
+            browser.DirectoryUnits.Add(directoryUnit);
+        }
+
+        foreach (var file in files)
+        {
+            var fileUnit = new FileUnit
+            {
+                Name = Path.GetFileName(file),
+                ModifyTime = Directory.GetLastAccessTime(file),
+                Size = ByteSize.FromBytes(new FileInfo(file).Length)
+            };
+            browser.FileUnits.Add(fileUnit);
+        }
+
+        return View(browser);
+    }
+
+
+}
---
+++ Models/FileBrowserUnit.cs
@@ -0,0 +1,35 @@
+﻿using ByteSizeLib;
+using HostServer.Extentions;
+
+namespace HostServer.Models
+{
+    public class FileBrowserUnit
+    {
+        public string? BasePath { get; set; }
+        public string? RelativePath { get; set; }
+        public string? Path
+        {
+            get
+            {
+                if(BasePath is null || RelativePath is null)
+                    return null;
+                return BasePath.MergePath(RelativePath);
+            }
+        }
+        public List<FileUnit> FileUnits { get; set; } = new();
+        public List<DirectoryUnit> DirectoryUnits { get; set; } = new();
+    }
+
+    public class FileUnit
+    {
+        public string? Name { get; set; }
+        public DateTime? ModifyTime { get; set; }
+        public ByteSize? Size { get; set; }
+    }
+
+    public class DirectoryUnit
+    {
+        public string? RelativePath { get; set; }
+        public DateTime? ModifyTime { get; set; }
+    }
+}
